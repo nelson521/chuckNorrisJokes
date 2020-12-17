43d92e60d0a4624ee589363797777b389d3d67c1
Stevenson Nelson <sonson521@gmail.com>
2020-12-17 00:22:56 -0500

    added multi jokes to http.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const number = document.querySelector('#number');
 const button = document
   .querySelector('#button')
   .addEventListener('click', getJoke);
 
 function getJoke(e) {
-  console.log(number.value);
+  const xhr = new XMLHttpRequest();
+  const number = document.querySelector('#number').value;
+
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const result = JSON.parse(this.responseText);
+      console.log(result);
+      console.log(number);
+    }
+  };
+  xhr.send();
   e.preventDefault();
 }
